d781978df2ebecb8c15a43c17a11a45a70c110c6
AntonGuzenko <=>
2022-02-06 20:30:40 +0200

    added tags for buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberFromScreen:Double = 0
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        result.text = result.text! + String(sender.tag)
+        numberFromScreen = Double(result.text!)!
+    }
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text != "" {
+            if sender.tag == 13 {
+            }
+            else if sender.tag == 14 {
+                
+            }
+            else if sender.tag == 15 {
 
+            }
+            else if  sender.tag == 16 {
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
