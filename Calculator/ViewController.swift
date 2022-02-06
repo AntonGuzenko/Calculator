af569b5bac9671962ddb54a84fededb4d81db105
AntonGuzenko <=>
2022-02-06 20:09:22 +0200

    Create View buttons

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Антон Гузенко on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
